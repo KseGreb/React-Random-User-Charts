e48011d3567b769d0f9bffd31db565ff8e224250
“KseGreb” <ksegreb@gmail.com>
2024-01-08 11:12:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Charts from './Charts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      getUser()
+  }, []);
+
+const getUser = async () => {
+      const res = await fetch('https://randomuser.me/api');
+      const {results} = await res.json();
+      setData(results);
+      console.log(results)
 }
 
-export default App;
+//If to use it without button NEXT, hen we can just use this code:
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch('https://randomuser.me/api');
+  //     const {results} = await res.json();
+  //     setData(results);
+  //     console.log(results)
+  //   };
+  //   fetchData();
+  // }, []);
+
+  return <>
+  <h2> Random User</h2>
+     {(data.map((item,index)=>{
+       return(
+         <>
+         <div className="card" key={index}>
+           <img src={item.picture.large} />
+           <h3>{item.name.title} {item.name.first} {item.name.last}</h3>
+           <p style={{textTransform: "capitalize"}} >{item.gender}  ( {item.dob.age} )</p>
+           <p><strong>City :</strong>{item.location.city} <strong>State :</strong> {item.location.state} <strong>Country :</strong>{item.location.country}</p>
+           <p>&#9993; {item.email}</p>
+           <p>&#128222; {item.phone} | {item.cell}</p>
+
+         
+        <button onClick={getUser} > NEXT </button>
+        </div>
+         </>
+       )
+     }))}
+     <div className="card">
+        <Charts/>
+     </div>
+     
+  </>
+}
